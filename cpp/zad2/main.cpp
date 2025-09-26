54d67323094a1d2fad8ee0e76d3e6c3a5c9cef90
szpontdeluxe <wmakos_221i1@e.zseeim.edu.pl>
2025-09-26 13:36:17 +0000

    studentmachine send

---
+++ cpp/zad2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int liczba1 = '0';
+int liczba2 = '0';
+char x ='e';
+while (x!='q'){
+cout << "kliknij q by zakonczyc program";
+cin>>x;
+if (liczba1<0)
+{
+}
+cout << "podaj liczby";
+cin>>liczba1;
+cin>>liczba2;
+liczba2+=liczba1;
+cout << "wynik:"; 
+cout << liczba2 ;
+}
+}
