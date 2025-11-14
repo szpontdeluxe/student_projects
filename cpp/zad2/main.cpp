5654e4dfadeb70f069593d408a44b1bea244937f
szpontdeluxe <wmakos_221i1@e.zseeim.edu.pl>
2025-11-14 14:35:29 +0000

    wyslanie pracy

--- cpp/zad2/main.cpp
+++ cpp/zad2/main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main(){
-int liczba1 = '0';
-int liczba2 = '0';
-char x ='e';
-while (x!='q'){
-cout << "kliknij q by zakonczyc program";
-cin>>x;
-if (liczba1<0)
-{
-}
-cout << "podaj liczby";
-cin>>liczba1;
-cin>>liczba2;
-liczba2+=liczba1;
-cout << "wynik:"; 
-cout << liczba2 ;
+	string imie;
+	string nazwisko;
+	cout<<"Podaj swoje imie"<<endl;
+	cin>>imie;
+	cout<<"Podaj swoje nazwisko"<<endl;
+	cin>>nazwisko;
+	fstream plik;
+	plik.open("dane.txt",ios::app);
+	if (!plik.is_open()){
+	cout<<"Nie działa";
+	return 1;
+	}
+
+	plik<<imie + nazwisko;
+	plik.close();
+	return 0;
+	cout<<imie + "" + nazwisko + "\n";
+	cout<<"czy dodac jeszcze jakies dane??";
+	char x = "t";
+	cin>>x;
+	string nowe;
+	if (x===t){
+	
 }
 }
