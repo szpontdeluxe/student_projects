0dd4aa3eacc0f38415c1cc5344e2368b284610d7
szpontdeluxe <wmakos_221i1@e.zseeim.edu.pl>
2025-11-21 14:23:12 +0000

    wyslanie pracy

--- cpp/zad3/szablon/main.cpp
+++ cpp/zad3/szablon/main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 
 using namespace std;
-ifstream plik;
+
+int main(){
+ifstream plik("dane.txt");
 plik.open("dane.txt");
 if (!plik.is_open()) {
     cout << "Nie udalo sie otworzyc pliku\n";
+	return 1;
 }
 
     int linia;
     int suma=0;
     while (getline(plik, linia)) {
         cout << linia << endl;
-        suma+=liczba;
-      
+        suma+=linia;
     }
-    cout "Suma wynosi:";
+    cout<< "Suma wynosi:";
       cout<<suma<<endl;
     plik.close();
     return 0;
